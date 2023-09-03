346d0a3305599e886a1e3e7f2254cbe474e1d1a6
Gilbert Sanchez <me@gilbertsanchez.com>
2023-09-03 04:00:19 +0000

    Compile and publish

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,3 +7,5 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [0.1.0] Unreleased
 
+First stab at using Log4NetParse module to parse Chocolatey. First version will
+just read the Chocolatey log(s).
--- ChocoLogParse/ChocoLogParse.psm1
+++ ChocoLogParse/ChocoLogParse.psm1
@@ -1,14 +1,1 @@
-# Dot source public/private functions
-$enums = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Enum/*.ps1') -Recurse -ErrorAction Stop)
-$classes = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Classes/*.ps1') -Recurse -ErrorAction Stop)
-$public = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Public/*.ps1')  -Recurse -ErrorAction Stop)
-$private = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Private/*.ps1') -Recurse -ErrorAction Stop)
-foreach ($import in @($enums + $classes + $public + $private)) {
-    try {
-        . $import.FullName
-    } catch {
-        throw "Unable to dot source [$($import.FullName)]"
-    }
-}
-
-Export-ModuleMember -Function $public.Basename
+# This module is compiled into one file. This will be appended at the end.
